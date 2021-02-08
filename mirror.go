a997495796ab5a57eea470886890558256b9e30f
Cyrill Burgener <cyrill.burgener@getyourguide.com>
2021-02-08 10:23:10 +0100

    Wait 2s on rate limit

--- mirror.go
+++ mirror.go
@@ -291,13 +291,17 @@ func (m *mirror) getRemoteTags() ([]RepositoryTag, error) {
 
         for retries > 0 {
             r, err = httpClient.Get(url)
-            if err != nil {
+
+            if err != nil && r.StatusCode < 429 {
+                log.Warningf("Rate limited, sleeping")
+                time.Sleep(2 * time.Second)
+                continue
+            }
+
+            if err != nil || r.StatusCode < 200 || r.StatusCode >= 300 {
                 log.Warningf("Failed to get %s, retrying", url)
                 retries -= 1
             } else {
-                if r.StatusCode < 200 || r.StatusCode >= 300 {
-                    log.Warningf("Status code %d while getting tags", r.StatusCode)
-                }
                 break
             }
         }
